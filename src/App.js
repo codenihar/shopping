e6f90942f5b7b1df8b601a8a6b3d8e5c330533ce
Varun <vvkulkarni360@gmail.com>
2024-03-17 00:04:29 +0530

    search implemented rawly

--- src/App.js
+++ src/App.js
@@ -1,17 +1,37 @@
-import React from "react";
-import {Routes,Route, BrowserRouter} from 'react-router-dom'
+import React, { useEffect, useState } from "react";
+import { Routes, Route, BrowserRouter, Router } from 'react-router-dom'
 import Home from "./components/Home/Home";
 import Login from "./components/Login/Login";
 import Products from "./components/Products/Products";
+import Navbar from "./components/NavBar/Navbar";
+import { products } from "./components/Products/ProductData";
+
 
 const App = () => {
+  const [productsState, setProductsState] = useState([]);
+  const [filteredProducts, setFilteredProducts] = useState([]);
+  
+  useEffect(() => {
+    setProductsState(products);
+    setFilteredProducts(products);
+  }, []);
+
+  const handleSearchInputChange = (query) => {
+    const filtered = products.filter(product=>
+      product.name.toLowerCase().includes(query.toLowerCase())
+    );
+    setFilteredProducts(filtered)
+  }
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home auth={{isAuth:true}}/>}></Route>
-        <Route path="/login" element={<Login auth={{isAuth:true}}/>}></Route>
-        <Route path="/products" element={<Products />} />
-      </Routes>
+      <Navbar onSearchInputChange={handleSearchInputChange} />
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<Home auth={{ isAuth: true }} />}></Route>
+          <Route path="/login" element={<Login auth={{ isAuth: true }} />}></Route>
+          <Route path="/products" element={<Products productsState={filteredProducts} />} />
+        </Routes>
+      </div>
     </BrowserRouter>
   );
 };
---
+++ src/components/NavBar/Navbar.js
@@ -0,0 +1,53 @@
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+
+const Navbar = (props) => {
+    const {onSearchInputChange} = props
+    const handleInputChange=(e)=>{
+        const query=e.target.value;
+        onSearchInputChange(query)
+    }
+    return (
+        <div>
+            <nav className="navbar navbar-expand-lg bg-body-tertiary">
+                <div className="container-fluid">
+                    <Link className="navbar-brand" to="/">Navbar</Link>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/">Home</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/products">Products</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/login" >Login</Link>
+                            </li>
+                            <li className="nav-item dropdown">
+                                <Link className="nav-link dropdown-toggle" to="/" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    Dropdown
+                                </Link>
+                                <ul className="dropdown-menu">
+                                    <li><Link className="dropdown-item" to="/">Action</Link></li>
+                                    <li><Link className="dropdown-item" to="/">Another action</Link></li>
+                                    <li><Link className="dropdown-item" to="/">Something else here</Link></li>
+                                </ul>
+                            </li>
+                        </ul>
+                        <form className="d-flex" role="search">
+                            <input className="form-control me-2" type="text" placeholder="Search" aria-label="Search" onChange={handleInputChange} />
+                            <button className="btn btn-outline-success" type="submit">Search</button>
+                        </form>
+                    </div>
+                </div>
+            </nav>
+        </div>
+    )
+}
+
+export default Navbar
+
+
