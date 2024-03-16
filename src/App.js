97a993b2e8d3d639c3c617abd0b5119658d8b52b
Varun <vvkulkarni360@gmail.com>
2024-03-17 00:21:35 +0530

    search refined-1

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import React, { useEffect, useState } from "react";
-import { Routes, Route, BrowserRouter, Router } from 'react-router-dom'
+import { Routes, Route, BrowserRouter } from 'react-router-dom'
 import Home from "./components/Home/Home";
 import Login from "./components/Login/Login";
 import Products from "./components/Products/Products";
 import Navbar from "./components/NavBar/Navbar";
-import { products } from "./components/Products/ProductData";
+import { productsData } from "./components/Products/ProductData";
 
 
 const App = () => {
-  const [productsState, setProductsState] = useState([]);
+  const [products, setProducts] = useState([]);
   const [filteredProducts, setFilteredProducts] = useState([]);
   
   useEffect(() => {
-    setProductsState(products);
-    setFilteredProducts(products);
+    setProducts(productsData);
+    setFilteredProducts(productsData);
   }, []);
 
   const handleSearchInputChange = (query) => {
@@ -29,7 +29,7 @@ const App = () => {
         <Routes>
           <Route path="/" element={<Home auth={{ isAuth: true }} />}></Route>
           <Route path="/login" element={<Login auth={{ isAuth: true }} />}></Route>
-          <Route path="/products" element={<Products productsState={filteredProducts} />} />
+          <Route path="/products" element={<Products products={filteredProducts} />} />
         </Routes>
       </div>
     </BrowserRouter>
--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -1,14 +1,13 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './Products.css';
-// import Products_nav from './Products_nav';
 
 function Products(props) {
-    const {productsState} = props
+    const {products} = props
     return (
         <div>
             {/* <h1 className="page-title">Products</h1> */}
             <div className="product-container">
-                {productsState.map(product => (
+                {products.map(product => (
                     <div key={product.id} className="product-card">
                         <img src={product.image} alt={product.name} />
                         <h3>{product.name}</h3>
@@ -25,24 +24,3 @@ function Products(props) {
 export default Products;
 
 
-
-{/* <Products_nav/> */ }
-{/* <div className="Nav-div">
-<nav className="navbar navbar-expand-lg bg-body-tertiary">
-    <div className="container-fluid">
-        <a className="navbar-brand" href="/">Products</a>
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                <li className="nav-item">
-                    <a className="nav-link active" aria-current="page" href="/">Home</a>
-                </li>
-                <li className="nav-item">
-                    <a className="nav-link" href="/">Link</a>
-                </li>
-            </ul>
-            <form className="d-flex" role="search">
-                <input className="form-control me-2" type="text" placeholder="Search" aria-label="Search" onChange={(e) => setSearch(e.target.value)}/>
-                {/* <button className="btn btn-outline-success" type="submit">Search</button></form></div></div></nav></div> */}
