3aaa113122edeb6f13957643f248ecf4b5a84c0c
sisindri <sisindrisingamsetti@gmail.com>
2024-05-23 14:58:55 +0530

    product card ui with all the contents 23.05.2024 14:58

--- src/index.js
+++ src/index.js
@@ -1,20 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { Auth0Provider } from '@auth0/auth0-react';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Auth0Provider } from "@auth0/auth0-react";
+import App from "./App";
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-      <Auth0Provider
-    domain="masscoders.us.auth0.com"
-    clientId="E6vqDYAuuashE9Kb1ixlZ6YWrcjs9jFf"
-    authorizationParams={{
-      redirect_uri: window.location.origin
-    }}
-  >
-    <App />
-  </Auth0Provider>
+    <Auth0Provider
+      domain="masscoders.us.auth0.com"
+      clientId="E6vqDYAuuashE9Kb1ixlZ6YWrcjs9jFf"
+      authorizationParams={{
+        redirect_uri: window.location.origin,
+      }}
+    >
+      <App />
+    </Auth0Provider>
   </React.StrictMode>
 );
